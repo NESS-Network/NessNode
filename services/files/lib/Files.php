b62787d3fcd71a96f2e0b0f0f29baf17c43cb2ff
Aleksej Sokolov <aleksej000@gmail.com>
2022-09-06 04:56:36 +0000

    Shadowname support

---
+++ services/files/lib/Files.php
@@ -0,0 +1,129 @@
+<?php
+namespace services\files\lib;
+
+use services\files\exceptions\ECantCreatePath;
+use services\files\exceptions\EConfigError;
+
+class Files {
+
+    public static function loadConfig (): array 
+    {
+        $files_config = require __DIR__ . '/../../../config/files.php';
+
+        if (!isset($files_config['quota'])) {
+            throw new EConfigError("files.json", "quota");
+        }
+
+        if (!isset($files_config['dir'])) {
+            throw new EConfigError("files.json", "dir");
+        }
+
+        return $files_config;
+    }
+
+    public static function translateQuota (string $quota): int 
+    {
+        $quota_size = (int) $quota;
+
+        if (0 === $quota_size) {
+            throw new \Exception("Quota size can not be zero");
+        }
+
+        if (strpos($quota, 'kb')) {
+            $quota_size = 1024 * $quota_size;
+        } elseif (strpos($quota, 'mb')) {
+            $quota_size = 1048576 * $quota_size;
+        } elseif (strpos($quota, 'gb')) {
+            $quota_size = 1073741824 * $quota_size;
+        }
+
+        return $quota_size;
+    }
+
+    public static function checkStoragePath () 
+    {
+        $files_config = self::loadConfig();
+        $dir = $files_config['dir'];
+
+        if (DIRECTORY_SEPARATOR !== substr($dir, 0, 1)) {
+            $dir = __DIR__ . '/../' . $dir;
+        }
+
+        if (!file_exists($dir)) {
+            if (!mkdir($dir)) {
+                throw new ECantCreatePath($dir);
+            }
+        }
+    }
+
+    public static function checkUserPath (string $username): string 
+    {
+        $files_config = self::loadConfig();
+        $dir = $files_config['dir'];
+
+        if (DIRECTORY_SEPARATOR !== substr($dir, 0, 1)) {
+            $dir = __DIR__ . '/../' . $dir;
+        }
+
+        $dir = $dir . '/' . $username;
+
+        if (!file_exists($dir)) {
+            if (!mkdir($dir)) {
+                throw new ECantCreatePath($dir);
+            }
+        }
+
+        return $dir;
+    }
+
+    public static function calcSpace (string $username): int 
+    {
+        $dir = self::checkUserPath($username);
+        $bytes = 0;
+
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
+
+        foreach ($iterator as $i) 
+        {
+            $bytes += $i->getSize();
+        }
+
+        return $bytes;
+    }
+
+    public static function listFiles (string $username): array|bool 
+    {
+        $dir = self::checkUserPath($username);
+
+        return glob($dir . '/*.*');
+    }
+
+    public static function fileinfo (string $username, string $filename): array|bool 
+    {
+        $userpath = self::checkUserPath($username);
+
+        if (false === $userpath) {
+            return false;
+        }
+
+        $fullname = $userpath . DIRECTORY_SEPARATOR . $filename;
+
+        // filename, size, id
+
+        return [$filename => [
+            'size' => filesize($fullname),
+            'id' => 0
+        ]];
+    }
+
+    public static function filesize (string $username, string $filename): int|bool 
+    {
+        $userpath = self::checkUserPath($username);
+
+        if (false === $userpath) {
+            return false;
+        }
+
+        return filesize($userpath . DIRECTORY_SEPARATOR . $filename);
+    }
+}
---
+++ services/files/lib/Upload.php
@@ -0,0 +1,9 @@
+<?php
+namespace services\files\lib;
+
+use services\files\exceptions\ECantCreatePath;
+use services\files\exceptions\EConfigError;
+
+class Upload {
+
+}
